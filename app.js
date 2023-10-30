85b907f413469ff250b3db12aeacd4e464e6a58b
McMartinezc <psyence_mc@hotmail.com>
2023-10-30 13:38:12 +0100

    fetch norris

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
 const image = document.querySelector('img');
 const jokeDIV = document.querySelector('#display-joke');
 const button = document.querySelector('#get-joke');
+
+button.addEventListener("click", ()=>{
+    getRandomJoke();
+})
+
+async function getRandomJoke() {
+    const respuesta = await fetch("https://api.chucknorris.io/jokes/random");
+    const datos = await respuesta.json();
+    jokeDIV.textContent = datos.value;
+}
+
